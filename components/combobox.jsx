12fadf004d5ed28c06e44bc29c04f21c2f102897
nickel-dime <goepnik@gmail.com>
2023-07-12 04:07:48 -0400

    i beg i beg

--- components/combobox.jsx
+++ components/combobox.jsx
@@ -31,6 +31,11 @@ const Example = ({ setClose, setPlayerSelected, boxId }, ref) => {
   }
 
   useEffect(() => {
+    setTimeout(() => {
+      var myElement = document.getElementsByTagName("input")[0];
+      console.log(myElement);
+      focusAndOpenKeyboard(myElement);
+    }, 50);
     loadPreviousGuesses();
   }, []);
 
@@ -110,6 +115,33 @@ const Example = ({ setClose, setPlayerSelected, boxId }, ref) => {
     }
   };
 
+  function focusAndOpenKeyboard(el, timeout) {
+    if (!timeout) {
+      timeout = 100;
+    }
+    if (el) {
+      // Align temp input element approximately where the input element is
+      // so the cursor doesn't jump around
+      var __tempEl__ = document.createElement("input");
+      __tempEl__.style.position = "absolute";
+      __tempEl__.style.top = el.offsetTop + 7 + "px";
+      __tempEl__.style.left = el.offsetLeft + "px";
+      __tempEl__.style.height = 0;
+      __tempEl__.style.opacity = 0;
+      // Put this temp element as a child of the page <body> and focus on it
+      document.body.appendChild(__tempEl__);
+      __tempEl__.focus();
+
+      // The keyboard is open. Now do a delayed focus on the target element
+      setTimeout(function () {
+        el.focus();
+        el.click();
+        // Remove the temp element
+        document.body.removeChild(__tempEl__);
+      }, timeout);
+    }
+  }
+
   return (
     <div className="top-16 w-80 sm:w-96">
       <Combobox
@@ -163,9 +195,7 @@ const Example = ({ setClose, setPlayerSelected, boxId }, ref) => {
                   !open && e.target.nextSibling.click();
                 }}
               />
-              <Combobox.Button
-                className="absolute inset-y-0 right-0 flex items-center pr-2"
-              >
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
                 <MagnifyingGlassIcon
                   className="h-5 w-5 text-black"
                   aria-hidden="true"
