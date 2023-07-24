e58584098ece3cce9ff70b25fa306d49f2143c53
nickel-dime <goepnik@gmail.com>
2023-07-24 13:44:39 -0400

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import { AppWrapper } from "./wrapper";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.connectgridgame.com/"),
@@ -51,6 +52,24 @@ export default function RootLayout({
           content="NFL, NBA, and MLB trivia based on the viral Immaculate Grid trend with different modes and settings!"
         />
       </head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LK38JHK9LT', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <body>
         <AppWrapper>{children}</AppWrapper>
         <Analytics />
